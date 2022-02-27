a63b8572a667469bf59143dc07e98f6b4b85ff9b
Trevor Mearns <spiral272@gmail.com>
2022-02-27 13:15:08 +0900

    added chat dummy data and returning to frontend

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,23 @@
 const express = require('express');
-const dotenv = require('dotenv');
+//const dotenv = require('dotenv');
 const app = express();
-dotenv.config();
+const { chats } = require('./data/data');
+//dotenv.config();
+// const connectDB = require('./config/db');
+// connectDB();
+
 app.get('/', (req, res) => {
   res.send('API is running.');
+});
+
+app.get('/api/chat', (req, res) => {
+  res.send(chats);
+  console.log('Chats: ', chats);
+});
+
+app.get('/api/chat/:id', (req, res) => {
+  const singleChat = chats.find((chat) => chat._id === req.params.id);
+  res.send(singleChat);
 });
 
 const PORT = process.env.PORT || 5000;
