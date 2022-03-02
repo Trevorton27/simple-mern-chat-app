9baaa10e050793b7bd174f44447a4b94446615a8
Trevor Mearns <spiral272@gmail.com>
2022-03-02 10:03:49 +0900

    add user/chat controller and route

--- backend/server.js
+++ backend/server.js
@@ -15,15 +15,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/user', require('./serverRoutes/userRoute'));
-// app.get('/api/chat', (req, res) => {
-//   res.send(chats);
-// });
-
-// app.get('/api/chat/:id', (req, res) => {
-//   const singleChat = chats.find((chat) => chat._id === req.params.id);
-//   res.send(singleChat);
-//   console.log('singleChat: ', singleChat);
-// });
+app.use('/api/chat', chatRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
