4496773ec4cb3a064d0553de6994b3e4dee3c90c
Trevor Mearns <spiral272@gmail.com>
2022-03-02 16:15:42 +0900

    create group chat working on back end

--- backend/serverRoutes/chatRoute.js
+++ backend/serverRoutes/chatRoute.js
@@ -1,10 +1,15 @@
 const express = require('express');
-const { accessChat, fetchChats } = require('../controllers/chatController');
+const {
+  accessChat,
+  fetchChats,
+  createGroupChat
+} = require('../controllers/chatController');
 const { protect } = require('../middleware/auth');
 
 const router = express.Router();
 
 router.route('/').post(protect, accessChat);
 router.route('/').get(protect, fetchChats);
+router.route('/group').post(protect, createGroupChat);
 
 module.exports = router;
